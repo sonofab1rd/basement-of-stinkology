84bbcd4503b62dd8f6944393fdb547c638a436b8
Christopher Dierkens <cjdierkens@gmail.com>
2021-01-17 18:10:29 -0500

    Wrap server create for local and lambda execution

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,11 +1,62 @@
+import { INestApplication } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { Context } from 'aws-lambda';
+import { createServer, proxy, Response } from 'aws-serverless-express';
+import * as dotenv from 'dotenv';
+import * as express from 'express';
+import { Express } from 'express';
+import { Server } from 'http';
 import { AppModule } from './app.module';
+import * as path from 'path';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000, () => {
-    console.log('API listening on http://localhost:3000');
+dotenv.config();
+console.log(`Loaded .env from ${path.resolve(process.cwd(), '.env')}`);
+
+export async function createApp(
+  expressApp: Express,
+): Promise<INestApplication> {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressApp),
+  );
+
+  return app;
+}
+
+async function bootstrap(): Promise<Server> {
+  const expressApp = express();
+
+  const app = await createApp(expressApp);
+  await app.init();
+
+  return createServer(expressApp);
+}
+
+let server: Server;
+
+/*
+ * Handler for AWS events
+ */
+export async function handler(event: any, context: Context): Promise<Response> {
+  if (!server) {
+    server = await bootstrap();
+  }
+
+  return proxy(server, event, context, 'PROMISE').promise;
+}
+
+/*
+ * Main function for traditional nodejs express server.
+ */
+async function main() {
+  const server = await bootstrap();
+
+  server.listen(3000, () => {
+    console.log('Server listening on http://localhost:3000');
   });
 }
 
-bootstrap();
+if (process.env.BOS_ENV === 'local') {
+  main();
+}
