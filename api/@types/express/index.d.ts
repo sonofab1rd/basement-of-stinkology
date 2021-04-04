87cedba3c94098f660ee683aa74c8252a24958c9
Christopher Dierkens <christopher.dierkens@formidable.com>
2021-04-03 22:26:54 -0400

    cleanup

--- api/@types/express/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { AuthUser } from 'src/auth/auth.service';
-
-declare global {
-  namespace Express {
-    interface Request {
-      authUser?: AuthUser;
-    }
-  }
-}
--- api/src/auth/constants.ts
+++
@@ -1,5 +0,0 @@
-console.log('process.env.BOS_JWT_SECRET', process.env.BOS_JWT_SECRET);
-
-export const jwtConstants = () => ({
-  secret: process.env.BOS_JWT_SECRET,
-});
