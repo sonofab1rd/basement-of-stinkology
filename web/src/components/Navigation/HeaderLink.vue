6df8af02cb0662da70461414a84222f3ab0aaffe
sonofab1rd <jacobdierkens@Jacobs-MacBook-Pro-2.local>
2021-04-07 23:20:07 -0400

    clean up link and styling

--- web/src/components/Navigation/HeaderLink.vue
+++ web/src/components/Navigation/HeaderLink.vue
@@ -1,31 +1,19 @@
 <template>
-  <div class="p-4 m-4">
-    <nuxt-link :to="getLink">
-      <div class="items-center text-center">
-        <Sofa v-if="link === 'The Basement'" class="block m-auto" />
-        <div v-else-if="link === 'Games'">Games</div>
-        <div v-else-if="link === 'Meals'">Meals</div>
-        <div v-else-if="link === 'Drinks'">Drinks</div>
-        <div v-else-if="link === 'Movies'">Movies</div>
-        <User v-else-if="link === 'auth'" class="block m-auto" />
-      </div>
-    </nuxt-link>
-    <!-- <nuxt-link v-else class="text-3xl m-2" :to="getLink">{{ link }}</nuxt-link> -->
-  </div>
+  <nuxt-link class="p-4 m-4 block" :to="getLink">
+    <Home v-if="link === 'The Basement'" />
+    <User v-else-if="link === 'auth'" />
+    <span v-else>{{ link }}</span>
+  </nuxt-link>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@nuxtjs/composition-api';
-import Sofa from '@/components/SVG/Sofa.vue';
-import Controller from '@/components/SVG/Controller.vue';
-import Burger from '@/components/SVG/Burger.vue';
-import Mug from '@/components/SVG/Mug.vue';
-import Reel from '@/components/SVG/Reel.vue';
 import User from '@/components/SVG/User.vue';
+import Home from '~/components/SVG/Home.vue';
 
 export default defineComponent({
   name: 'HeaderLink',
-  components: { Sofa, Controller, Burger, Mug, Reel, User },
+  components: { Home, User },
   props: {
     link: {
       type: String,
