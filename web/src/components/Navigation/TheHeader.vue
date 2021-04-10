8b381ab1af3a12933b974b316615e5a59fd6536a
Christopher Dierkens <christopher.dierkens@formidable.com>
2021-04-10 17:37:59 -0400

    make header links reactive

--- web/src/components/Navigation/TheHeader.vue
+++ web/src/components/Navigation/TheHeader.vue
@@ -3,7 +3,7 @@
     <header>
       <TheSidebarToggle @toggle="$emit('sidenavToggle')" />
       <ul class="hidden bg-gray-300 md:grid grid-cols-6 place-items-center">
-        <li v-for="link in links" :key="link">
+        <li v-for="link in navigation.links" :key="link">
           <HeaderLink :link="link" />
         </li>
       </ul>
@@ -12,23 +12,41 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, useStore } from '@nuxtjs/composition-api';
+import {
+  computed,
+  defineComponent,
+  reactive,
+  useStore,
+  watch,
+} from '@nuxtjs/composition-api';
 import HeaderLink from '@/components/Navigation/HeaderLink.vue';
 import TheSidebarToggle from '@/components/Navigation/TheSidebarToggle.vue';
+
+const getLinks = (isAuthenticated: boolean) => [
+  '/',
+  'Games',
+  'Meals',
+  'Drinks',
+  'Movies',
+  isAuthenticated ? 'user-profile' : 'auth',
+];
 
 export default defineComponent({
   name: 'TheHeader',
   components: { HeaderLink, TheSidebarToggle },
   setup() {
     const store = useStore();
-    const isAuthenticated: Boolean = store.getters.isAuthenticated;
-    const links: Array<String> = ['/', 'Games', 'Meals', 'Drinks', 'Movies'];
-    if (isAuthenticated) {
-      links.push('user-profile');
-    } else {
-      links.push('auth');
-    }
-    return { links };
+    const isAuthenticated = computed<boolean>(
+      () => store.getters.isAuthenticated,
+    );
+
+    const navigation = reactive({ links: getLinks(isAuthenticated.value) });
+
+    watch(isAuthenticated, (value) => {
+      navigation.links = getLinks(value);
+    });
+
+    return { navigation };
   },
 });
 </script>
