4f13b75a07331ef96b9fc790eec330d5e94cce72
sonofab1rd <jacobdierkens@Jacobs-MBP-2.neo.rr.com>
2021-04-25 11:44:01 -0400

    clean up form, add weekendOptions to store actions

--- web/src/pages/date-plan/index.vue
+++ web/src/pages/date-plan/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="text-center" style="text-align: -webkit-center">
-    <h2 class="text-xl font-extrabold">Select dates in order of preference!</h2>
+    <h2 class="text-xl font-extrabold mt-1">
+      Select dates in order of preference!
+    </h2>
     <form @submit.prevent="onSubmit">
-      <div v-for="(option, index) in options" :key="index" class="block">
-        <label class="block pt-5 text-lg font-bold">
-          Option {{ index + 1 }}</label
-        >
-        <app-control-input class="inline-block" type="date"
+      <div v-for="(option, index) in options" :key="index">
+        <div class="pt-3 text-lg font-bold">Option {{ index + 1 }}</div>
+        <app-control-input
+          v-model="options[index].start"
+          class="inline-block"
+          type="date"
           >Start Date</app-control-input
         >
-        <app-control-input class="inline-block" type="date"
+        <app-control-input
+          v-model="options[index].end"
+          class="inline-block"
+          type="date"
           >End Date</app-control-input
         >
       </div>
@@ -19,20 +25,21 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from '@nuxtjs/composition-api';
+import { defineComponent, reactive, useStore } from '@nuxtjs/composition-api';
 import AppButton from '~/components/UI/AppButton.vue';
 import AppControlInput from '~/components/UI/AppControlInput.vue';
 
 export default defineComponent({
   components: { AppControlInput, AppButton },
   setup() {
+    const store = useStore();
     const options = reactive([
-      { start: new Date(), end: new Date() },
-      { start: new Date(), end: new Date() },
-      { start: new Date(), end: new Date() },
+      { start: '', end: '' },
+      { start: '', end: '' },
+      { start: '', end: '' },
     ]);
     const onSubmit = () => {
-      console.log('options', options);
+      store.dispatch('weekendOptions', { ...options });
     };
 
     return {
