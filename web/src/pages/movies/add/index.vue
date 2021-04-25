d090e5672891cd446ace153c3c1190559c120947
sonofab1rd <jacobdierkens@Jacobs-MBP-2.neo.rr.com>
2021-04-25 15:57:39 -0400

    add tmdb keys and start search form

---
+++ web/src/pages/movies/add/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="text-center" style="text-align: -webkit-center">
+    <h1 class="text-xl font-extrabold mt-1">Add a movie</h1>
+    <form @submit.prevent="onSubmit">
+      <app-control-input class="block" type="text">Name</app-control-input>
+      <app-control-input class="block" type="text">Actor</app-control-input>
+      <div>
+        <app-button type="submit">Search</app-button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, useStore, reactive } from '@nuxtjs/composition-api';
+import AppButton from '~/components/UI/AppButton.vue';
+import AppControlInput from '~/components/UI/AppControlInput.vue';
+
+export default defineComponent({
+  components: { AppControlInput, AppButton },
+  setup() {
+    const store = useStore();
+    const state = reactive({ searchData: { name: '', actor: '' } });
+    const onSubmit = () => {
+      store.dispatch('getMovie', state);
+    };
+    return { onSubmit };
+  },
+});
+</script>
--- web/src/pages/movies/index.vue
+++ web/src/pages/movies/index.vue
@@ -1,16 +1,22 @@
 <template>
-  <div>
-    <p>Movies page!</p>
+  <div class="text-center">
+    <app-button class="mt-3" @click="addMovie">Add a Movie!</app-button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api';
+import { defineComponent, useRouter } from '@nuxtjs/composition-api';
+import AppButton from '~/components/UI/AppButton.vue';
 
 export default defineComponent({
+  components: { AppButton },
   middleware: ['check-auth', 'auth'],
   setup() {
-    return {};
+    const router = useRouter();
+    const addMovie = () => {
+      router.push('/movies/add');
+    };
+    return { addMovie };
   },
 });
 </script>
